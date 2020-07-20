d0f5c4be20d140093c97b118cb22d05a3014d901
Michelle Rolak <michellerolak7@gmail.com>
2020-07-20 13:22:11 -0400

    add palindrome

--- python/challenges/strings/palindrome.py
+++ python/challenges/strings/palindrome.py
@@ -8,8 +8,33 @@ def palindrome(str1):
 	else:
 		return palindrome(str1[1:len(str1)-1])
 
+#check if a string is a permutation of a palindrome 
 
-print(palindrome("racecar"))
-print(palindrome("tattarrattat"))
-print(palindrome("racerrecar"))
-print(palindrome("area"))
+def perm_of_pal(str1):
+	#dictionary to count occurence of characters
+	dic = {}
+	#remove white space chars
+	str1 = str1.replace(' ','')
+
+	#iterate through each char and put them into the dict
+	for char in str1:
+		if char not in dic:
+			dic[char] = 1
+		else:
+			dic[char]+=1
+	#check how many odd values exist, if there is more than 1 this cannot be a palindrome
+	oddCount = 0
+	for key in dic:
+		if dic[key]%2 != 0:
+			oddCount+=1
+
+	if len(str1)%2 != 0 and oddCount == 1:
+		return True
+	elif oddCount == 0 and len(str1)%2 == 0:
+		return True
+	else:
+		return False
+
+print(perm_of_pal("atco cta"))
+print(perm_of_pal("taco cat"))
+print(perm_of_pal("taco car"))
