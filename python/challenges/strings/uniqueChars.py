83b6175da028e524c337c97a7b3816bea3667c5f
Michelle Rolak <michellerolak7@gmail.com>
2020-07-20 11:09:15 -0400

    add uniqueChars

--- python/challenges/strings/uniqueChars.py
+++ python/challenges/strings/uniqueChars.py
@@ -19,3 +19,26 @@ def uniqueChars(string):
 uniqueChars("racecar")
 uniqueChars("abcdefg")
 
+#implement this method without using additional data structures 
+
+#one way to approach this problem without the use of additional data strcutures would be to use
+# a double for loop to traverse the string and compare it with every other element of the string, 
+# breaking if there is a match. But this algorithm takes O(n^2) 
+
+#an alternative approach would be to sort the string, then check for 'matching neighbors'
+
+def unique_chars(string):
+
+	#sorted() creates a list of sorted chars
+	sorted_list = (sorted(string))
+
+	#loop through the sorted_list and check to see if neighbors match
+	for i in range(0, len(sorted_list)-1):
+		if sorted_list[i] == sorted_list[i+1]:
+			print(string, " contains duplicate chars")
+			return
+	print(string, " is unique")
+
+unique_chars("abcde")
+unique_chars("racecar")
+
